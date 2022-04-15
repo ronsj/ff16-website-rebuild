1339f416c70228bb8590082f3ea11c9894b649af
Ron San Jose <ronsj1@gmail.com>
2022-04-15 00:34:01 -0700

    add section animations

---
+++ src/components/icons/SquareEnix.js
@@ -0,0 +1,24 @@
+export default function SquareEnix() {
+  return (
+    <svg
+      viewBox="0 0 748 78"
+      style={{
+        enableBackground: 'new 0 0 748 78',
+      }}
+      xmlSpace="preserve"
+    >
+      <style>{'.logo_sqex_01{fill:#fff}.logo_sqex_02{fill:#e20a16}'}</style>
+      <path
+        className="logo_sqex_01"
+        d="M97 0c-6.6 0-12 5.4-12 12v45c0 6.6 5.4 12 12 12h16v9h15v-9h17c6.6 0 12-5.4 12-12V12c0-6.6-5.4-12-12-12H97zm3 12h41v44h-13v-7h-15v7h-13V12zM14 0C7.4 0 2 5.4 2 12v16c0 6.6 5.4 12 12 12h46v16H0v1c0 6.6 5.4 12 12 12h49c6.6 0 12-5.4 12-12V39c0-6.6-5.4-12-12-12H15V12h43v6h14v-6c0-6.6-5.4-12-12-12H14zM169 0v57c0 6.6 5.4 12 12 12h43c6.6 0 12-5.4 12-12V0h-15v56h-37V0h-15zM273.5 1 239 69h15l7.6-15h38.8l7.6 15h15L288.5 1l-.5-1h-14l-.5 1zm7.5 14.8L294.3 42h-26.6L281 15.8zM330 0v69h15V43h9.5l25 26h20l-25-26h7.5c6.6 0 12-5.4 12-12V12c0-6.6-5.4-12-12-12h-52zm15 12h30c2.2 0 4 1.8 4 4v11c0 2.2-1.8 4-4 4h-30V12zM407 0v69h58V56h-43V12h43V0h-58z"
+      />
+      <path className="logo_sqex_02" d="M435 27h25v13h-25z" />
+      <path className="logo_sqex_01" d="M497 0v69h58V56h-43V12h43V0h-58z" />
+      <path className="logo_sqex_02" d="M525 27h25v13h-25z" />
+      <path
+        className="logo_sqex_01"
+        d="M567 0v69h14V20.2l36 42.9 1 1.2 3.9 4.7H632V0h-15v46.4L581 3.5 578.1 0H567zM644 0h16v69h-16zM667.9 0l30 33.3L667.8 69h18l21.5-25.3L730.1 69h18L716 33.3 744.2 0h-18l-19.6 23.1L685.9 0h-18z"
+      />
+    </svg>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,22 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import { CSSProperties, ReactNode } from 'react'
+import { CSSProperties, ReactNode, useEffect, useRef, useState } from 'react'
 import Logo from '../components/icons/Logo'
 import Facebook from '../components/icons/Facebook'
 import Instagram from '../components/icons/Instagram'
 import Twitter from '../components/icons/Twitter'
 import PS5 from '../components/icons/PS5'
-import { motion } from 'framer-motion'
+import SquareEnix from '../components/icons/SquareEnix'
+import { useAnimation, motion } from 'framer-motion'
+import { useInView } from 'react-intersection-observer'
 import styles from '../styles/Home.module.css'
 
-const Home: NextPage = () => {
-  const mediaQuery = {
-    lg: '(min-width: 821px) and (orientation: landscape)',
-  }
+const mediaQuery = {
+  lg: '(min-width: 821px) and (orientation: landscape)',
+}
 
+const Home: NextPage = () => {
   return (
     <>
       <Head>
@@ -26,195 +28,202 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       {/* Hero */}
-      <section className={styles.hero}>
-        <picture>
-          <source
-            media={mediaQuery.lg}
-            srcSet="/images/home_hero_landscape.jpeg"
-          />
-          <img
-            src="/images/home_hero_portrait.jpeg"
-            alt="Final Fantasy XVI art"
-          />
-        </picture>
-        <div className={styles.hero_heading}>
-          <h1>
-            <Logo />
-          </h1>
+      <header className={styles.page_header}>
+        <SquareEnix />
+        <PS5 />
+      </header>
+      <main>
+        <section className={styles.hero}>
+          <picture>
+            <source
+              media={mediaQuery.lg}
+              srcSet="/assets/images/na/kv/kv_pc.jpg"
+            />
+            <img
+              className={styles.hero_image}
+              src="/assets/images/na/kv/kv_sp.jpg"
+              alt="Final Fantasy XVI art"
+            />
+          </picture>
+          <div className={styles.hero_heading}>
+            <h1>
+              <Logo />
+            </h1>
+            <p>
+              The legacy of the crystals has shaped our history for long enough.
+            </p>
+          </div>
+        </section>
+        <SectionContainer>
+          <div className={styles.trailer_button}>
+            <picture>
+              <source
+                media={mediaQuery.lg}
+                srcSet="/assets/images/na/trailer/trailer_playbtn_pc.png"
+              />
+              <img
+                src="/assets/images/na/trailer/trailer_playbtn_sp.png"
+                alt="open video modal"
+              />
+            </picture>
+          </div>
+          <div className={styles.trailer_frame}>
+            <picture>
+              <source
+                media={mediaQuery.lg}
+                srcSet="/assets/images/na/trailer/trailer_frame_pc.png"
+              />
+              <img
+                src="/assets/images/na/trailer/trailer_frame_sp.png"
+                role="presentation"
+              />
+            </picture>
+          </div>
+          <div className={styles.trailer_thumbnail}>
+            <picture>
+              <source
+                media={mediaQuery.lg}
+                srcSet="/assets/images/na/trailer/trailer_thumbnail_pc.jpg"
+              />
+              <img
+                src="/assets/images/na/trailer/trailer_thumbnail_sp.jpg"
+                alt="final fantasy 16 awakening trailer thumbnail"
+              />
+            </picture>
+          </div>
+        </SectionContainer>
+        <SectionContainer>
+          <div className={styles.link_block}>
+            <picture>
+              <source
+                media={mediaQuery.lg}
+                srcSet="/assets/images/na/common/world_pc.jpg"
+              />
+              <img
+                src="/assets/images/na/common/world_sp.jpg"
+                alt="final fantasy 16 awakening trailer thumbnail"
+              />
+            </picture>
+            <div className={styles.link_block_cta}>
+              <h2>
+                The <span className={styles.kearning_xs}>W</span>orld
+              </h2>
+              <DetailsButton />
+            </div>
+          </div>
+        </SectionContainer>
+        <SectionContainer>
+          <div className={styles.link_block}>
+            <picture>
+              <source
+                media={mediaQuery.lg}
+                srcSet="/assets/images/na/common/character_pc.jpg"
+              />
+              <img
+                src="/assets/images/na/common/character_sp.jpg"
+                alt="final fantasy 16 awakening trailer thumbnail"
+              />
+            </picture>
+            <div className={styles.link_block_cta}>
+              <h2>The Characters</h2>
+              <DetailsButton />
+            </div>
+          </div>
+        </SectionContainer>
+        <SectionContainer style={{ textAlign: 'center' }}>
+          <h2>Social Media</h2>
           <p>
-            The legacy of the crystals has shaped our history for long enough.
+            Follow these accounts to receive
+            <br />
+            the latest updates.
           </p>
-        </div>
-      </section>
-      <SectionContainer>
-        <div className={styles.trailer_button}>
-          <picture>
-            <source
-              media={mediaQuery.lg}
-              srcSet="/assets/images/na/trailer/trailer_playbtn_pc.png"
+          <ul className={styles.social_media_links}>
+            <li>
+              <ExternalLink href="https://twitter.com/finalfantasyxvi">
+                <Twitter />
+                <p>Twitter</p>
+              </ExternalLink>
+            </li>
+            <li>
+              <ExternalLink href="https://www.instagram.com/finalfantasyxvi/">
+                <Instagram />
+                <p>Instagram</p>
+              </ExternalLink>
+            </li>
+            <li>
+              <ExternalLink href="https://www.facebook.com/finalfantasyxvi/">
+                <Facebook />
+                <p>Facebook</p>
+              </ExternalLink>
+            </li>
+          </ul>
+          <div className={styles.social_media_links_ps5}>
+            <PS5 />
+          </div>
+        </SectionContainer>
+        <footer className={styles.section_container}>
+          <div className={styles.footer_game_rating}>
+            <ExternalLink href="https://esrb.com">
+              <img
+                src="/assets/images/na/footer/rating01.png"
+                alt="ESRB rating pending"
+              />
+            </ExternalLink>
+            <ExternalLink href="https://esrb.org/EPCConfirm/907/">
+              <img
+                src="/assets/images/na/footer/rating02.png"
+                alt="ESRB privacy certified"
+              />
+            </ExternalLink>
+          </div>
+          <ul className={styles.footer_links}>
+            <li>
+              <ExternalLink href="https://square-enix-games.com/en_US/documents/cookies">
+                Cookie Notice
+              </ExternalLink>
+            </li>
+            <li>
+              <ExternalLink href="https://square-enix-games.com/en_US/documents/tnc">
+                Terms of Use
+              </ExternalLink>
+            </li>
+            <li>
+              <ExternalLink href="https://square-enix-games.com/en_US/documents/privacy">
+                Privacy Notice
+              </ExternalLink>
+            </li>
+            <li>
+              <ExternalLink href="https://square-enix-games.com/en_US/documents/materialusagepolicy">
+                Material Usage Policy
+              </ExternalLink>
+            </li>
+          </ul>
+          <p className={styles.footer_copy}>
+            © SQUARE ENIX CO., LTD. ALL RIGHTS RESERVED. <br />
+            LOGO ILLUSTRATION: © 2020 YOSHITAKA AMANO
+          </p>
+          <p className={styles.footer_trademark}>
+            FINAL FANTASY, SQUARE ENIX and their respective logos are trademarks
+            or registered trademarks of Square Enix Holdings Co., Ltd.
+            <br />
+            &quot;PlayStation&quot; and the &quot;PS&quot; Family logo are
+            registered trademarks and &quot;PS5&quot; is a trademark of Sony
+            Interactive Entertainment Inc.
+          </p>
+        </footer>
+        <div className={styles.firespark}>
+          <div className={styles.firespark_video_wrapper}>
+            <FiresparkLoopingVideo
+              className={styles.firespark_video_portrait}
+              src="/assets/videos/firespark-portrait.mp4"
             />
-            <img
-              src="/assets/images/na/trailer/trailer_playbtn_sp.png"
-              alt="open video modal"
+            <FiresparkLoopingVideo
+              className={styles.firespark_video_landscape}
+              src="/assets/videos/firespark-landscape.mp4"
             />
-          </picture>
-        </div>
-        <div className={styles.trailer_frame}>
-          <picture>
-            <source
-              media={mediaQuery.lg}
-              srcSet="/assets/images/na/trailer/trailer_frame_pc.png"
-            />
-            <img
-              src="/assets/images/na/trailer/trailer_frame_sp.png"
-              role="presentation"
-            />
-          </picture>
-        </div>
-        <div className={styles.trailer_thumbnail}>
-          <picture>
-            <source
-              media={mediaQuery.lg}
-              srcSet="/assets/images/na/trailer/trailer_thumbnail_pc.jpg"
-            />
-            <img
-              src="/assets/images/na/trailer/trailer_thumbnail_sp.jpg"
-              alt="final fantasy 16 awakening trailer thumbnail"
-            />
-          </picture>
-        </div>
-      </SectionContainer>
-      <SectionContainer>
-        <div className={styles.link_block}>
-          <picture>
-            <source
-              media={mediaQuery.lg}
-              srcSet="/assets/images/na/common/world_pc.jpg"
-            />
-            <img
-              src="/assets/images/na/common/world_sp.jpg"
-              alt="final fantasy 16 awakening trailer thumbnail"
-            />
-          </picture>
-          <div className={styles.link_block_cta}>
-            <h2>
-              The <span className={styles.kearning_xs}>W</span>orld
-            </h2>
-            <DetailsButton />
           </div>
         </div>
-      </SectionContainer>
-      <SectionContainer>
-        <div className={styles.link_block}>
-          <picture>
-            <source
-              media={mediaQuery.lg}
-              srcSet="/assets/images/na/common/character_pc.jpg"
-            />
-            <img
-              src="/assets/images/na/common/character_sp.jpg"
-              alt="final fantasy 16 awakening trailer thumbnail"
-            />
-          </picture>
-          <div className={styles.link_block_cta}>
-            <h2>The Characters</h2>
-            <DetailsButton />
-          </div>
-        </div>
-      </SectionContainer>
-      <SectionContainer style={{ textAlign: 'center' }}>
-        <h2>Social Media</h2>
-        <p>
-          Follow these accounts to receive
-          <br />
-          the latest updates.
-        </p>
-        <ul className={styles.social_media_links}>
-          <li>
-            <ExternalLink href="https://twitter.com/finalfantasyxvi">
-              <Twitter />
-              <p>Twitter</p>
-            </ExternalLink>
-          </li>
-          <li>
-            <ExternalLink href="https://www.instagram.com/finalfantasyxvi/">
-              <Instagram />
-              <p>Instagram</p>
-            </ExternalLink>
-          </li>
-          <li>
-            <ExternalLink href="https://www.facebook.com/finalfantasyxvi/">
-              <Facebook />
-              <p>Facebook</p>
-            </ExternalLink>
-          </li>
-        </ul>
-        <div className={styles.social_media_links_ps5}>
-          <PS5 />
-        </div>
-      </SectionContainer>
-      <footer className={styles.section_container}>
-        <div className={styles.footer_game_rating}>
-          <ExternalLink href="https://esrb.com">
-            <img
-              src="/assets/images/na/footer/rating01.png"
-              alt="ESRB rating pending"
-            />
-          </ExternalLink>
-          <ExternalLink href="https://esrb.org/EPCConfirm/907/">
-            <img
-              src="/assets/images/na/footer/rating02.png"
-              alt="ESRB privacy certified"
-            />
-          </ExternalLink>
-        </div>
-        <ul className={styles.footer_links}>
-          <li>
-            <ExternalLink href="https://square-enix-games.com/en_US/documents/cookies">
-              Cookie Notice
-            </ExternalLink>
-          </li>
-          <li>
-            <ExternalLink href="https://square-enix-games.com/en_US/documents/tnc">
-              Terms of Use
-            </ExternalLink>
-          </li>
-          <li>
-            <ExternalLink href="https://square-enix-games.com/en_US/documents/privacy">
-              Privacy Notice
-            </ExternalLink>
-          </li>
-          <li>
-            <ExternalLink href="https://square-enix-games.com/en_US/documents/materialusagepolicy">
-              Material Usage Policy
-            </ExternalLink>
-          </li>
-        </ul>
-        <p className={styles.footer_copy}>
-          © SQUARE ENIX CO., LTD. ALL RIGHTS RESERVED. <br />
-          LOGO ILLUSTRATION: © 2020 YOSHITAKA AMANO
-        </p>
-        <p className={styles.footer_trademark}>
-          FINAL FANTASY, SQUARE ENIX and their respective logos are trademarks
-          or registered trademarks of Square Enix Holdings Co., Ltd.
-          <br />
-          &quot;PlayStation&quot; and the &quot;PS&quot; Family logo are
-          registered trademarks and &quot;PS5&quot; is a trademark of Sony
-          Interactive Entertainment Inc.
-        </p>
-      </footer>
-      <div className={styles.firespark}>
-        <div className={styles.firespark_video_wrapper}>
-          <FiresparkLoopingVideo
-            className={styles.firespark_video_portrait}
-            src="/assets/videos/firespark-portrait.mp4"
-          />
-          <FiresparkLoopingVideo
-            className={styles.firespark_video_landscape}
-            src="/assets/videos/firespark-landscape.mp4"
-          />
-        </div>
-      </div>
+      </main>
     </>
   )
 }
@@ -225,10 +234,31 @@ type SectionContainerProps = {
 }
 
 function SectionContainer({ children, style }: SectionContainerProps) {
+  const controls = useAnimation()
+  const [ref, inView] = useInView()
+
+  useEffect(() => {
+    if (inView) {
+      controls.start('visible')
+    }
+  }, [controls, inView])
+
+  const variants = {
+    visible: { opacity: 1, top: 0, transition: { duration: 1 } },
+    hidden: { opacity: 0, top: '1rem' },
+  }
+
   return (
-    <section className={styles.section_container} style={style}>
+    <motion.section
+      ref={ref}
+      animate={controls}
+      initial="hidden"
+      variants={variants}
+      className={styles.section_container}
+      style={style}
+    >
       {children}
-    </section>
+    </motion.section>
   )
 }
 
